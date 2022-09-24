c92ae6f515272d6a7480535dfce541890a24fc53
23priya <99719180+23priya@users.noreply.github.com>
2022-09-23 22:56:06 -0700

    Create priyanka_kothule.cpp

---
+++ 2022-Sept/24 Sept 2022/priyanka_kothule.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n=5,i,j;
+  for(i=1;j<=5;i++)
+  {
+    for(j=1;j<=5;j++)
+    {
+      cout<<i;
+    }
+    cout<<endl;
+  }
+  return 0;
+}
