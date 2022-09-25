c55ccd2cd4f795c8d8e4ea28c5f1a631b709ffa3
pagaredivyanka <113618065+pagaredivyanka@users.noreply.github.com>
2022-09-25 09:57:40 +0530

    Create Divyanka_Pagare.java

---
+++ 2022-Sept/25 Sept 2022/Divyanka_Pagare.java
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter no: ";
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
