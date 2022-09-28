a22dcc61f54ee880075ccea346470f561b9575af
Rushikesh Bhalerao <93110597+rushirao17@users.noreply.github.com>
2022-09-28 22:19:27 +0530

    Create Rushikesh_Bhalerao.cpp

---
+++ 2022-Sept/28 Sept 2022/Rushikesh_Bhalerao.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,i,j,k;
+    cin>>n;
+    for(i=0;i<=n;i++){
+        for(z=0;z<i;z++){
+        cout<<" ";}
+
+        for(k=n;k>=1;k--){
+            char ch='A';
+            if(k<=n-i){
+            cout<<char(ch+k-1);}
+            else{
+                cout<<" ";
+            }
+        }
+        
+
+        for(j=1;j<=n-i;++j){
+            char ch='A';
+            cout<<char(ch+j-1);
+        }
+        
+        cout<<endl;
+    }
+}
