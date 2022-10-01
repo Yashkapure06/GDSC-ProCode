a13ab999b2a5ac5b8584df7203b0c9675077aec5
Kunal Nandre <111383966+kunalnandre@users.noreply.github.com>
2022-10-01 17:31:08 +0530

    Create Kunal_Nandre.java
    Solution for 01/10/22 Palindrome Question

---
+++ 2022-Oct/01 Oct 2022/Kunal_Nandre.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) 
+  {
+	  Scanner sc = new Scanner(System.in);
+	  String n= sc.nextLine();
+	  String rev="";
+	  for(int i=n.length()-1;i>=0;i--){
+	    rev=rev + n.charAt(i);}
+	
+	  if(rev.equals(n)){
+	    System.out.println("Palindrome");}
+	  else{
+	    System.out.println("Not palindrome");}
+	}
+}
