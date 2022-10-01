92d858720996ac0582fd349997996dc55a1c937a
Prathamesh Patil <91467888+PrathamP31@users.noreply.github.com>
2022-10-01 19:39:30 +0530

    Update palindrome.py

--- 2022-Oct/01 Oct 2022/palindrome.py
+++ 2022-Oct/01 Oct 2022/palindrome.py
@@ -1,9 +1,14 @@
+# Author : Prathamesh Patil
+
+# getting the input string
 strs = input()
 
 # print(str)
 
+# using the slicing concept for revering the input string
 rev_strs = strs[::-1]
 
+# comparing if the input string is equal to the reverse string 
 if strs == rev_strs:
   print("Plaindrome")
 else:
