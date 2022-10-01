70e6a73ed9bbec5fd80b494f3331332a655f528c
VaishnaviNGosavi <114799341+VaishnaviNGosavi@users.noreply.github.com>
2022-10-01 15:04:35 +0530

    Create vaishnavi_gosavi.java

---
+++ vaishnavi_gosavi.java
@@ -0,0 +1,40 @@
+/*Problem Statement : 01 Oct 2022
+Name:Vaishnavi Gosavi
+
+- Accept a String from user & check if Palindrome or NOT<br>
+Input : "ABC"
+Output : NOT Palindrome
+
+Input : "ABBCCBBA"
+Output : Palindrome
+
+Input : "RACECAR"
+Output : Palindrome
+
+Inout : "step on no pets"
+Output : Palindrome
+*/
+
+import java.util.*;
+
+class vaishnavi_gosavi {
+    public static void main(String[] args) {
+        String St, Rev = "";
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input:");
+        St = sc.nextLine();
+
+        int length = St.length();
+
+        for (int i = length - 1; i >= 0; i--) {
+            Rev += St.charAt(i);
+        }
+        if (St.equals(Rev)) {
+            System.out.println("Output:Palindrome");
+        } else {
+            System.out.println("Output:Not Palindrome");
+        }
+
+    }
+}
