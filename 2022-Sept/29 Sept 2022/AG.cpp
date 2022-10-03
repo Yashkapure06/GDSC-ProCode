2de4e1ee5a7f5e65728e456da43e294f1bc267cc
ADITI GULATI <83702433+aditi-gulati@users.noreply.github.com>
2022-10-03 12:56:34 +0530

    Create AG.cpp

---
+++ 2022-Sept/29 Sept 2022/AG.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n;j++){
+            char ch='z';
+            if(j<=n-i+1){cout<<char(ch-j+1)<<" ";}
+            else{cout<<" ";}
+        }
+        cout<<endl;
+    }
+}
