fdccc4985ef7534814019937718eea9283ba5045
Kunal Nandre <111383966+kunalnandre@users.noreply.github.com>
2022-10-08 07:47:28 +0530

    Create Kunal_Nandre.java

---
+++ 2022-Oct/07 Oct 2022/Kunal_Nandre.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+public class Kunal_Nandre
+{
+
+	 static boolean checkPalin(String word)
+    {
+        int n = word.length();
+        word = word.toLowerCase();
+ 
+        for (int i = 0; i < n; i++, n--)
+            if (word.charAt(i) !=
+                       word.charAt(n - 1))
+                return false;
+        return true;
+    }
+    
+    static String longestPalin(String str)
+    {
+        str = str + " ";
+ 
+        String longestword = "", word = "";
+ 
+        int length, length1 = 0;
+        for (int i = 0; i < str.length(); i++)
+        {
+            char ch = str.charAt(i);
+
+            if (ch != ' ')
+                word = word + ch;
+            else {
+                length = word.length();
+                if (checkPalin(word) &&
+                             length > length1)
+                {
+                    length1 = length;
+                    longestword = word;
+                }
+ 
+                word = "";
+            }
+        }
+ 
+        return longestword;
+    }
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+ 
+        if (longestPalin(s) == "")
+            System.out.println("No Palindrome"
+                            + " Word");
+        else
+            System.out.println(longestPalin(s));
+    }
+    
+}
