7b547fca15ffd6df4eac63fb8c1442bd5e81fe12
Kunal Nandre <111383966+kunalnandre@users.noreply.github.com>
2022-10-08 07:48:28 +0530

    Update Kunal_Nandre.java

--- 2022-Oct/07 Oct 2022/Kunal_Nandre.java
+++ 2022-Oct/07 Oct 2022/Kunal_Nandre.java
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 import java.util.Scanner;
 public class Kunal_Nandre
 {
