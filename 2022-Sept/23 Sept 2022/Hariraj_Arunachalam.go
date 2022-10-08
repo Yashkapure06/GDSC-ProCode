bb26196de6d40af4a4ef088695749c99fa7233c7
Hariraj <raja70a@gmail.com>
2022-10-08 22:33:20 +0530

    Pattern Created by Hariraj (#238)

---
+++ 2022-Sept/23 Sept 2022/Hariraj_Arunachalam.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+  var n int
+  fmt.Print("Enter the value of n: ")
+  fmt.Scanf("%d", &n)
+  for i := 1; i <= n; i++ {
+    for j := 1; j <= n ; j++ {
+      fmt.Print(j, " ")
+    }
+    fmt.Println()
+  }  
+}
