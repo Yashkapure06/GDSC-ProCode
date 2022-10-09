5ac120c2b6e0504f615894049f1ab4b4821ac388
Riya Gupta <61340238+Caffeine-Overflows@users.noreply.github.com>
2022-10-09 23:13:34 +0530

    Create riya_gupta.py

---
+++ 2022-Oct/09 Oct 2022/riya_gupta.py
@@ -0,0 +1,17 @@
+lst = []
+
+def print_combinations(str1, str2, index):
+    if index == len(str1):
+        lst.append(str2)
+        return
+    print_combinations(str1, str2, index+1)
+    print_combinations(str1, str2+str1[index], index+1)
+
+def main():
+    str1 = input("Enter a string: ")
+    print_combinations(str1, "", 0)
+
+if __name__ == "__main__":
+    main()
+    for i in range(len(lst)-1, -1, -1):
+        print(lst[i]+" ", end="")
