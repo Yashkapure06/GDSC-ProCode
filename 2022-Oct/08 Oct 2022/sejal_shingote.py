79f555cffda662d527434d983f05d1075eb2b396
sejals2 <113519765+sejals2@users.noreply.github.com>
2022-10-12 17:37:55 +0530

    created by sejal_shingote of 8 oct

---
+++ 2022-Oct/08 Oct 2022/sejal_shingote.py
@@ -0,0 +1,17 @@
+def toString(List): 
+    return ''.join(List) 
+  
+def permute(a, l, r): 
+    if l==r: 
+        print (toString(a))
+    else: 
+        for i in range(l,r): 
+            a[l], a[i] = a[i], a[l] 
+            permute(a, l+1, r) 
+            a[l], a[i] = a[i], a[l] # backtrack 
+  
+string = "ABC"
+n = len(string) 
+a = list(string) 
+permute(a, 0, n) 
+    
