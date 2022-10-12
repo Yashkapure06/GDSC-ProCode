71dcfaed2fba75e239a414e90ab81b8fb95a306c
avniwani11 <110182304+avniwani11@users.noreply.github.com>
2022-10-12 16:47:09 +0530

    Problem solved for 11th october in python

---
+++ 2022-Oct/11 Oct 2022/avani_11.py
@@ -0,0 +1,11 @@
+def isPalindrome(string):
+    if string == string[ ::-1]:
+        return True
+str = input("----Please Enter a string---- :\n")
+count = 0
+
+for word in str.split():
+    if isPalindrome(word):
+        count += 1
+
+print("There are",count,"Palindrome Strings in the word")
