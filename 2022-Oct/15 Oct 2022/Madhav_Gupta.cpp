867721ffe07037f776bec4e0503a14a5025630a5
Madhav Gupta <72290706+madhavgupta07@users.noreply.github.com>
2022-10-15 09:34:28 +0530

    Create Madhav_Gupta.cpp

---
+++ 2022-Oct/15 Oct 2022/Madhav_Gupta.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int rotate(int arr[], int n){
+    int key = arr[0];
+    for(int i=0; i<n; i++){
+        arr[i] = arr[i+1];
+    }
+    arr[n-1] = key;
+}
+int main()
+{
+    int arr[100];
+    int n;
+    cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    int d;
+    cin>>d;
+    while(d--){
+        rotate(arr,n);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }        
+    return 0;
+}
