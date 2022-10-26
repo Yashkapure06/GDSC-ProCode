df9a8bf60cfaac33c65d0f222db2bce95103a2b7
Aaditya Said <aadityasaid@gmail.com>
2022-10-26 21:59:07 +0530

    feat: add solution for 03 oct 2022

---
+++ 2022-Oct/03 Oct 2022/aaditya_said.py
@@ -0,0 +1,17 @@
+#changing uppercase to lowercase to uppercase
+def string(string):
+    
+    new_string=string.swapcase()
+    print(new_string)
+    
+
+string("tEsT")
+string("mEt GdsC")
+string("CoDER")
+
+
+
+
+
+
+
